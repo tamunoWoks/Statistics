5056c621ec4b12076844b44b52c35e7642365c77
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2025-07-04 09:29:31 -0600

    Define median function

--- median.py
+++ median.py
@@ -1,1 +1,6 @@
-
+def median(data):
+    """
+    Returns the median of a list of numbers.
+    The median is the middle value in an ordered list.
+    If the list has an even number of elements, the median is the average of the two middle values.
+    """
