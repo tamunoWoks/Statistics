226c799bf76f3915503af9416ef1ec0477de0efc
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2025-07-04 09:56:21 -0600

    Define mode function

--- mode.py
+++ mode.py
@@ -1,1 +1,6 @@
-
+def mode(data):
+    """
+    Returns the mode(s) of a list of numbers.
+    The mode is the value(s) that appear most frequently.
+    If multiple values share the highest frequency, all are returned in a list.
+    """
