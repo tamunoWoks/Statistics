930ef9712327ff1106f272204c6c57412df98bc4
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2025-07-04 10:47:32 -0600

    Define mean function

--- std_dev.py
+++ std_dev.py
@@ -3,3 +3,9 @@ from math import sqrt
 
 # Sample dataset:
 data3 = [13.04, 1.32, 22.65, 17.44, 29.54, 23.22, 17.65, 10.12, 26.73, 16.43]
+
+def mean(data):
+    """
+    Calculates the mean (average) of a list of numbers.
+    """
+    return sum(data) / len(data)
