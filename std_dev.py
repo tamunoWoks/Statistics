4ee6e47c29724308b22526449d1a96183d146705
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2025-07-04 10:47:59 -0600

    Define median function

--- std_dev.py
+++ std_dev.py
@@ -9,3 +9,12 @@ def mean(data):
     Calculates the mean (average) of a list of numbers.
     """
     return sum(data) / len(data)
+
+def variance(data):
+    """
+    Calculates the variance of a list of numbers.
+    Variance measures the average squared deviation from the mean.
+    """
+    mu = mean(data)  # Compute the mean of the dataset
+    squared_diffs = [(x - mu) ** 2 for x in data]  # Square of each deviation from the mean
+    return mean(squared_diffs)  # Average of squared deviations
