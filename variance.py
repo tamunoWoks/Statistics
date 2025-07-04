82b5b76e7abeb74133bc9b417809d759b21f0a68
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2025-07-04 10:23:56 -0600

    Define variance()

--- variance.py
+++ variance.py
@@ -3,3 +3,11 @@ data3=[13.04, 1.32, 22.65, 17.44, 29.54, 23.22, 17.65, 10.12, 26.73, 16.43]
 # Define mean():
 def mean(data):
     return sum(data)/len(data)
+
+# Define variance():
+def variance(data):
+    """
+    Returns the variance of a list of numbers.
+    Variance measures how far each number in the list is from the mean.
+    Formula: sum((x - mean)^2) / n
+    """
