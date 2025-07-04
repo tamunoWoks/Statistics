f833c162aa5b4e6c5de9a4aa90ff4a3e7211a5c0
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2025-07-04 10:46:52 -0600

    Sample dataset

--- std_dev.py
+++ std_dev.py
@@ -1,3 +1,5 @@
-# Import square root function from the math module
+# Import square root function from the math module:
 from math import sqrt
 
+# Sample dataset:
+data3 = [13.04, 1.32, 22.65, 17.44, 29.54, 23.22, 17.65, 10.12, 26.73, 16.43]
