407b310dfca7b179a6d725c94173560b41f8afc8
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2025-07-16 09:18:53 -0600

    Update coin_flip_sampling.py

--- coin_flip_sampling.py
+++ coin_flip_sampling.py
@@ -3,3 +3,15 @@ import random
 from math import sqrt
 from plotting import * 
 
+# Function to calculate the mean (average) of a list of numbers
+def mean(data):
+    return float(sum(data)) / len(data)
+
+# Function to calculate the variance of a list of numbers
+def variance(data):
+    mu = mean(data)
+    return sum([(x - mu) ** 2 for x in data]) / len(data)
+
+# Function to calculate the standard deviation
+def stddev(data):
+    return sqrt(variance(data))
