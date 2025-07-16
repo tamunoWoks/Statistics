7b41bf0cfa0ad2d0046d56d5f10e7a04654668fd
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2025-07-16 09:12:04 -0600

    Function to calculate variance

--- coin_flip_stats.py
+++ coin_flip_stats.py
@@ -5,3 +5,8 @@ from math import sqrt
 # Function to calculate the mean (average) of a list of numbers
 def mean(data):
     return float(sum(data)) / len(data)
+
+# Function to calculate the variance of a list of numbers
+def variance(data):
+    mu = mean(data)  # Compute the mean first
+    return sum([(x - mu)**2 for x in data]) / len(data)
